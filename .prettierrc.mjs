c5d1d7b4692cf0e94a8a28009d304aeb88484c9e
Graham Hall <graham@ghall.dev>
2025-06-16 12:25:57 -0400

    deploy test

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,6 +2,12 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
   overrides: [
     {
       files: "*.astro",
