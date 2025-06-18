fe81f07ca2802b43c08bc9da0135c9c6e4ba49f1
Graham Hall <graham@ghall.dev>
2025-06-18 16:28:37 -0400

    contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
-import alpinejs from "@astrojs/alpinejs";
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations: [icon(), tailwind(), alpinejs({ entrypoint: "./entrypoint" })],
+	output: 'static',
+	integrations: [icon(), tailwind(), alpinejs({ entrypoint: './entrypoint' })],
 });
